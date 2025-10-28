761f0f8e79de4a43865345b5241774530ff5067b
Student <student@example.com>
2025-10-28 21:36:45 +0000

    updated activity_pet_profile.py

--- python_practice/activity_pet_profile.py
+++ python_practice/activity_pet_profile.py
@@ -1,24 +1,24 @@
 # Prompt the user for pet details.
 pet_name = input("Enter your pet's name: ")
-pet_type = # Use input() to collect this data.
-pet_age = # Use input() to collect this data.
+pet_type = input("Enter your pet's type: ")# Use input() to collect this data.
+pet_age = int(input("Enter your pet's age: "))# Use input() to collect this data.
 
 # Format the pet description using an f-string.
-pet_description = # Add f-string here.
+pet_description = f"{pet_name} is a {pet_age}-year-old {pet_type}."# Add f-string here. pet_age pet_type"
 
 # Convert the description to uppercase.
-uppercase_description = # Use .upper().
+uppercase_description = pet_description.upper()# Use .upper().
 
 # Ask how many treats the pet gets per day.
-treats_per_day = # Use input() to collect this data.
-treats_per_week = # Convert to integer and calculate treats per week.
+treats_per_day = int(input("How many treats does your pet get per day? "))# Use input() to collect this data.
+treats_per_week = treats_per_day * 7 # Convert to integer and calculate treats per week.
 
 # Replace the pet type.
-updated_description = # Use .replace() to modify pet type from "dog" to "canine".
-updated_description = # Use .replace() to modify pet type from "cat" to "feline".
+updated_description = f"{pet_name} is a {pet_age}-year-old {pet_type.replace("dog", "canine")}"# Use .replace() to modify pet type from "dog" to "canine".
+updated_description = f"{pet_name} is a {pet_age}-year-old {pet_type.replace("cat", "feline")}"# Use .replace() to modify pet type from "cat" to "feline".
 
 # Display a thank you message.
-thank_you_message = # Add escape characters here.
+thank_you_message = "Thanks for visiting! \nCome Again."# Add escape characters here.
 
 # Print all results.
 print("\nPet Profile:")
