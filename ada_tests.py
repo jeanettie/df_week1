ed9688c9d47e1466c256c89ce5ee39afbf0e6b3c
Student <student@example.com>
2025-10-30 00:38:31 +0000

    testing ada problems

---
+++ ada_tests.py
@@ -0,0 +1,55 @@
+# problem 3.1 S
+my_string1 = "I love Seattle"
+my_string1 = my_string1[7]
+
+# problem 3.2 lo
+my_string2 = "I love Seattle"
+my_string2 = my_string2[2:4]
+
+# problem 3.3 Ada Lovelace
+my_string3 = "Ada"
+my_string3 += " Lovelace"
+
+# problem 3.4 Ada codes it!
+my_string4 = "Ada"
+my_string4 += " codes" + " it!"
+
+# problem 3.5 Ada likes
+my_string5 = "Ada"
+my_string5 = (my_string5 + " likes to code")[4:9]
+
+# problem 3.6 Goodbye world!
+my_string6 = "Hello world"
+my_string6 = "Goodbye " + my_string6[6:11] + "!"
+
+# problem 3.7 Hello, goodbye!
+my_string7 = "Hello world!"
+my_string7 = my_string7[0:5] + ", goodbye!"
+
+# problem 3.8 Hi!
+my_string8 = "Hello world!"
+my_string8 = my_string8[:1] + "i" + "!"
+
+# problem 3.9 PythonloveI
+my_string9 = "I love Python"
+my_string9 = my_string9[7:13] + my_string9[2:6] + my_string9[0]
+
+# problem 3.10 Python rocks!
+my_string10 = "I love Python"
+my_string10 = "P" + my_string10[8:13] + " rocks!"
+
+# problem 3.11 lovePythonlove
+my_string11 = "I love Python"
+my_string11 = my_string11[2:6] + my_string11[7:13] + my_string11[2:6]
+
+print(my_string1)
+print(my_string2)
+print(my_string3)
+print(my_string4)
+print(my_string5)
+print(my_string6)
+print(my_string7)
+print(my_string8)
+print(my_string9)
+print(my_string10)
+print(my_string11)
